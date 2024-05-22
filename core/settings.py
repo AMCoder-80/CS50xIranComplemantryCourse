6aeb452568720c9fcdfaf43134795321fd862bc8
Alireza Mortezaei <alirezamortezaei50@gmail.com>
2024-05-22 20:11:53 +0330

    add a app named base with a simple view and url

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Local apps
+    "base.apps.BaseConfig",
+
+    # Third party apps
 ]
 
 MIDDLEWARE = [
