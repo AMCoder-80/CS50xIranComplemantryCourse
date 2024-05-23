c23fc22381aff7934abb5bf58669e7ba534c1f09
Alireza Mortezaei <alirezamortezaei50@gmail.com>
2024-05-23 23:47:56 +0330

    less complexity in exceptions.py

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,6 +1,5 @@
 from rest_framework.exceptions import APIException
 from rest_framework import status
-from django.utils.encoding import force_str
 
 
 class CustomException(APIException):
@@ -10,6 +9,6 @@ class CustomException(APIException):
     def __init__(self, detail, field, status_code):
         if status_code is not None:self.status_code = status_code
         if detail is not None:
-            self.detail = {field: force_str(detail)}
+            self.detail = {field: detail}
         
-        else: self.detail = {'detail': force_str(self.default_detail)}
+        else: self.detail = {'detail': self.default_detail}
