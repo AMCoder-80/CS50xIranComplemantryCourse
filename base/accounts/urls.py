a902614af74ca31733c005e0a81f63546fc78ec7
Alireza Mortezaei <alirezamortezaei50@gmail.com>
2024-05-24 00:45:40 +0330

    add login, refresh and verify routes from rest simple jwt builtin views

--- base/accounts/urls.py
+++ base/accounts/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 from base.accounts.views import (
     UserCreationView, ProfileCreationView
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
     # path('signup/', create_user, name="create_new_user"),   
     path('signup/', UserCreationView.as_view(), name="create_new_user"),
-    path('profile/create/', ProfileCreationView.as_view(), name="create_new_profile"),   
+    path('profile/create/', ProfileCreationView.as_view(), name="create_new_profile"),
+    # rest default routes
+    path('login/', TokenObtainPairView.as_view(), name='login_with_password'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
